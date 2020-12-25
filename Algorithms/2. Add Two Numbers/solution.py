8856a0b7c89f711e67074895f98725ffbe6e1eb9
ShaneKao <El3vul4cl3gj94>
2020-12-25 15:57:18 +0800

    Add Two Numbers

--- Algorithms/2. Add Two Numbers/solution.py
+++ Algorithms/2. Add Two Numbers/solution.py
@@ -13,35 +13,41 @@ class ListNode(object):
 
 
 class Solution(object):
+    def list_node_length(self, l):
+        return 0 if l.next is None else 1 + self.list_node_length(l.next)
+
     def addTwoNumbers(self, l1, l2):
         result_list = []
-        extra_num = 0
+        l1_len = self.list_node_length(l1)
+        l2_len = self.list_node_length(l2)
+        len_diff = l1_len - l2_len
         while True:
-            try:
-                l1_num = l1.pop()
-            except IndexError:
-                l1_num = 0
-            try:
-                l2_num = l2.pop()
-            except IndexError:
-                l2_num = 0
-            num = l1_num + l2_num + extra_num
-            extra_num = 1 if num >= 10 else 0
-            result_list.append(num%10)
-            if not len(l1) and not len(l2):
-                if extra_num == 1:
-                    result_list.append(1)
+            l1_val = l1.val if len_diff >= 0 else 0
+            l2_val = l2.val if len_diff <= 0 else 0
+            sum_val = l1_val + l2_val
+            # if len_diff < 0:
+            #     len_diff += 1
+            # if len_diff > 0:
+            #     len_diff -= 1
+            # print(len_diff)
+            print(sum_val)
+            if l1.next is None and l2.next is None:
                 break
-        return result_list
+            l1 = l1.next if len_diff >= 0 else l1
+            l2 = l2.next if len_diff <= 0 else l2
+
+
 
 
 if __name__ == '__main__':
-    l1 = [2, 4, 3]
-    l2 = [5, 6, 4]
-
-    l1 = [99]
-    l2 = [3]
     # l1 = ListNode(val=2, next=ListNode(val=4, next=ListNode(val=3, next=None)))
     # l2 = ListNode(val=5, next=ListNode(val=6, next=ListNode(val=4, next=None)))
-    # print(len(l1))
-    print(Solution().addTwoNumbers(l1=l1, l2=l2))
+    # Solution().addTwoNumbers(l1=l1, l2=l2)
+    #
+    # l1 = ListNode(val=0, next=None)
+    # l2 = ListNode(val=0, next=None)
+    # Solution().addTwoNumbers(l1=l1, l2=l2)
+
+    l1 = ListNode(val=9, next=ListNode(val=9, next=ListNode(val=9, next=ListNode(val=9, next=ListNode(val=9, next=ListNode(val=9, next=ListNode(val=9, next=None)))))))
+    l2 = ListNode(val=9, next=ListNode(val=9, next=ListNode(val=9, next=ListNode(val=9, next=None))))
+    Solution().addTwoNumbers(l1=l1, l2=l2)
