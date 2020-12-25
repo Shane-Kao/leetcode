1e3fea8529ee78142e9abc7168840afe6af32346
shanekao <shanekao19870615@gmail.com>
2020-12-25 23:06:26 +0800

    Add Two Numbers

--- Algorithms/2. Add Two Numbers/solution.py
+++ Algorithms/2. Add Two Numbers/solution.py
@@ -8,46 +8,81 @@ class ListNode(object):
         self.val = val
         self.next = next
 
-    def __len__(self):
-        return 0 if self.next is None else 1 + len(self.next)
-
 
 class Solution(object):
-    def list_node_length(self, l):
-        return 0 if l.next is None else 1 + self.list_node_length(l.next)
+    @staticmethod
+    def carry_digit(l):
+        while any([i >= 10 for i in l]):
+            for i, j in enumerate(l):
+                if j >= 10:
+                    l[i] = j - 10
+                    try:
+                        l[i + 1] += 1
+                    except IndexError:
+                        l.append(1)
+                    break
+        return l
+
 
     def addTwoNumbers(self, l1, l2):
         result_list = []
-        l1_len = self.list_node_length(l1)
-        l2_len = self.list_node_length(l2)
-        len_diff = l1_len - l2_len
         while True:
-            l1_val = l1.val if len_diff >= 0 else 0
-            l2_val = l2.val if len_diff <= 0 else 0
-            sum_val = l1_val + l2_val
-            # if len_diff < 0:
-            #     len_diff += 1
-            # if len_diff > 0:
-            #     len_diff -= 1
-            # print(len_diff)
-            print(sum_val)
-            if l1.next is None and l2.next is None:
+            try:
+                l1_val = l1.val
+                l1_end = False
+            except AttributeError:
+                l1_end = True
+                l1_val = 0
+            try:
+                l2_val = l2.val
+                l2_end = False
+            except AttributeError:
+                l2_end = True
+                l2_val = 0
+            if l1_end and l2_end:
                 break
-            l1 = l1.next if len_diff >= 0 else l1
-            l2 = l2.next if len_diff <= 0 else l2
-
-
+            sum_val = l1_val + l2_val
+            result_list.append(sum_val)
+            l1 = l1.next if not l1_end else l1
+            l2 = l2.next if not l2_end else l2
+        result_list = self.carry_digit(result_list)
+        next_ = None
+        for i in reversed(result_list):
+            next_ = ListNode(val=i, next=next_)
+        return result_list, next_
 
 
 if __name__ == '__main__':
-    # l1 = ListNode(val=2, next=ListNode(val=4, next=ListNode(val=3, next=None)))
-    # l2 = ListNode(val=5, next=ListNode(val=6, next=ListNode(val=4, next=None)))
-    # Solution().addTwoNumbers(l1=l1, l2=l2)
-    #
-    # l1 = ListNode(val=0, next=None)
-    # l2 = ListNode(val=0, next=None)
-    # Solution().addTwoNumbers(l1=l1, l2=l2)
+    l1 = ListNode(val=2, next=ListNode(val=4, next=ListNode(val=3, next=None)))
+    l2 = ListNode(val=5, next=ListNode(val=6, next=ListNode(val=4, next=None)))
+    result_list = Solution().addTwoNumbers(l1=l1, l2=l2)
+    print(result_list)
+
+    l1 = ListNode(val=0, next=None)
+    l2 = ListNode(val=0, next=None)
+    result_list = Solution().addTwoNumbers(l1=l1, l2=l2)
+    print(result_list)
 
     l1 = ListNode(val=9, next=ListNode(val=9, next=ListNode(val=9, next=ListNode(val=9, next=ListNode(val=9, next=ListNode(val=9, next=ListNode(val=9, next=None)))))))
     l2 = ListNode(val=9, next=ListNode(val=9, next=ListNode(val=9, next=ListNode(val=9, next=None))))
-    Solution().addTwoNumbers(l1=l1, l2=l2)
+    # 9999999 + 9999 = 10009998, [8, 9, 9, 9, 0, 0, 0, 1]
+    result_list = Solution().addTwoNumbers(l1=l1, l2=l2)
+    print(result_list)
+
+    l1 = ListNode(val=9, next=ListNode(val=2, next=ListNode(val=3, next=None)))
+    l2 = ListNode(val=4, next=ListNode(val=5, next=ListNode(val=6, next=None)))
+    # 329 + 654 = 983, [3, 8, 9]
+    result_list = Solution().addTwoNumbers(l1=l1, l2=l2)
+    print(result_list)
+
+    l1 = ListNode(val=1, next=ListNode(val=7, next=ListNode(val=3, next=None)))
+    l2 = ListNode(val=4, next=ListNode(val=5, next=ListNode(val=6, next=None)))
+    # 371 + 654 = 1025, [5, 2, 0, 1]
+    result_list = Solution().addTwoNumbers(l1=l1, l2=l2)
+    print(result_list)
+    
+    l1 = ListNode(val=7, next=ListNode(val=1, next=None))
+    l2 = ListNode(val=6, next=None)
+    # 17 + 6 = 23, [3, 2]
+    result_list = Solution().addTwoNumbers(l1=l1, l2=l2)
+    print(result_list)
