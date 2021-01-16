685e4cc25a553210586db42a3fdcaa5741a2b406
shanekao <shanekao19870615@gmail.com>
2021-01-16 15:15:11 +0800

    Longest Palindromic Substring

---
+++ Algorithms/5. Longest Palindromic Substring/solution.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Shane_Kao'
+import math
+import bisect
+from typing import List
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        N = len(s)
+        g = (s[i: i + len_] for len_ in reversed(range(1, N + 1)) for i in range(N - len_ + 1))
+        while True:
+            curr_ = next(g)
+            if curr_ == curr_[::-1]:
+                return curr_
+
+
+
+
+if __name__ == '__main__':
+    f = Solution().longestPalindrome
+    # print(f('babad'))
+    assert (f('babad') == 'bab')
+    assert (f('cbbd') == 'bb')
+    assert (f('a') == 'a')
+    assert (f('ac') == 'a')
+
+
