0b07616c4e7a83ad5bb38e9f128705fd7b19f680
ShaneKao <El3vul4cl3gj94>
2021-03-12 15:31:09 +0800

    add 11

--- Algorithms/11. Container With Most Water/solution.py
+++ Algorithms/11. Container With Most Water/solution.py
@@ -3,13 +3,20 @@ __author__ = 'Shane_Kao'
 
 
 class Solution:
-    def maxArea(self, height) -> int:
-        pass
-
-    @staticmethod
-    def _get_area(list_, start_idx, end_idx):
-        val_ = list_[start_idx] if list_[start_idx] < list_[end_idx] else list_[end_idx]
-        return val_ * (end_idx - start_idx)
+    def maxArea(self, height):
+        max_area = 0
+        i = 0
+        j = len(height) - 1
+        while i != j and i < len(height):
+            min_ = min(height[i], height[j])
+            curr_area = (j - i) * min_
+            if curr_area > max_area:
+                max_area = curr_area
+            if height[i] > height[j]:
+                j -= 1
+            else:
+                i += 1
+        return max_area
 
 
 if __name__ == '__main__':
@@ -18,3 +25,5 @@ if __name__ == '__main__':
     assert solution.maxArea(height=[1,8,6,2,5,4,8,3,7]) == 49
     assert solution.maxArea(height=[4,3,2,1,4]) == 16
     assert solution.maxArea(height=[1,2,1]) == 2
+    print(solution.maxArea(height=[2,3,4,5,18,17,6]))
+    # assert solution.maxArea(height=[2,3,4,5,18,17,6]) == 17
